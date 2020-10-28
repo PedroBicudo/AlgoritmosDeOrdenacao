68a13f8fdbe3cf51a781f612d80a0ef743386360
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-10-28 13:45:31 -0300

    Agora o plot tem uma classe própria.

--- src/index.py
+++ src/index.py
@@ -1,40 +1,64 @@
 from utils import FileUtils, TempoAlgoritmos
+from matplotlib import pyplot
+from typing import List
+import matplotlib
 import utils
-from matplotlib import pyplot as plt
-
-#Declaração de Variaveis e Parametros
-metodos = ['Bubble Sort', 'Binary Insertion Sort' ,'Quick Sort']
-plt.style.use("classic")
-fig, (ax1,ax2,ax3) = plt.subplots(3)
-#Começando a Contagem
-Archive = utils.FileUtils()
-valueArchive = Archive.getNumerosFromFile()
-Tempo = TempoAlgoritmos().obter_tempo_de_execucao_de_cada_algoritmo(valueArchive)
-print(Tempo)
-ax1.bar(metodos,Tempo)
 
 
-#Plotagem do graficos do arquivo de 5000 Numeros
-Tempo = []
-Archive = utils.FileUtils()
-valueArchive = Archive.getNumerosFromFile()
-Tempo = TempoAlgoritmos().obter_tempo_de_execucao_de_cada_algoritmo(valueArchive)
-print(Tempo)
-ax2.bar(metodos,Tempo)
+class PlotAlgoritmos:
+
+    ALGORITMOS = ['Bubble Sort', 'Binary Insertion Sort' ,'Quick Sort']
+    
+    def __init__(self):
+        self.file_utils = FileUtils()
+        self.tempo_algoritmos = TempoAlgoritmos()
+        self._definir_o_estilo_de_plot()
+        self._gerar_sub_plot_de_cada_quantidade_de_numeros()
+
+    def _definir_o_estilo_de_plot(self):
+        pyplot.style.use("classic")
+
+    def _gerar_sub_plot_de_cada_quantidade_de_numeros(self):
+        sub_plots = matplotlib.pyplot.subplots(3)
+        self.sub_plot_mil_numeros = sub_plots[1][0]
+        self.sub_plot_cinco_mil_numeros = sub_plots[1][1]
+        self.sub_plot_dez_mil_numeros = sub_plots[1][2]
+    
+    def mostrar_plot_de_todas_as_quantias_de_numeros(self):
+        self._definir_nomes_dos_labels_de_cada_sub_plot()
+        pyplot.show()
+
+    def _definir_nomes_dos_labels_de_cada_sub_plot(self):
+        self.sub_plot_mil_numeros.set(title="Grafico do Arquivo de 1000 Numeros", ylabel="Tempo")
+        self.sub_plot_cinco_mil_numeros.set(title="Grafico do Arquivo de 5000 Numeros", ylabel="Tempo")
+        self.sub_plot_dez_mil_numeros.set(title="Grafico do Arquivo de 10000 Numeros", ylabel="Tempo")
+    
+    def adicionar_tempo_de_mil_numeros_ao_plot(self):
+        tempos = self._obter_tempo_de_execucao_dos_algoritmos()
+        self.sub_plot_mil_numeros.bar(PlotAlgoritmos.ALGORITMOS, tempos)
+
+    def adicionar_tempo_de_cinco_mil_numeros_ao_plot(self):
+        tempos = self._obter_tempo_de_execucao_dos_algoritmos()
+        self.sub_plot_cinco_mil_numeros.bar(PlotAlgoritmos.ALGORITMOS, tempos)
+
+    def adicionar_tempo_de_dez_mil_numeros_ao_plot(self):
+        tempos = self._obter_tempo_de_execucao_dos_algoritmos()
+        self.sub_plot_dez_mil_numeros.bar(PlotAlgoritmos.ALGORITMOS, tempos)
+    
+    def _obter_tempo_de_execucao_dos_algoritmos(self) -> List[float]:
+        numeros = self.file_utils.getNumerosFromFile()
+        tempos = self.tempo_algoritmos.obter_tempo_de_execucao_de_cada_algoritmo(numeros)
+
+        return tempos
+
+def main():
+    plot = PlotAlgoritmos()
+    plot.adicionar_tempo_de_mil_numeros_ao_plot()
+    plot.adicionar_tempo_de_cinco_mil_numeros_ao_plot()
+    plot.adicionar_tempo_de_dez_mil_numeros_ao_plot()
+    plot.mostrar_plot_de_todas_as_quantias_de_numeros()
 
 
-#Plotagem do graficos do arquivo de 10000 Numeros
-Tempo = []
-Archive = utils.FileUtils()
-valueArchive = Archive.getNumerosFromFile()
-Tempo = TempoAlgoritmos().obter_tempo_de_execucao_de_cada_algoritmo(valueArchive)
-print(Tempo)
-ax3.bar(metodos,Tempo)
-
-
-ax1.set(title="Grafico do Arquivo de 1000 Numeros", ylabel="Tempo")
-ax2.set(title="Grafico do Arquivo de 5000 Numeros", ylabel="Tempo")
-ax3.set(title="Grafico do Arquivo de 10000 Numeros", ylabel="Tempo")
-
-plt.show()
-print(Tempo)
+if __name__ == '__main__':
+    main()
+    
