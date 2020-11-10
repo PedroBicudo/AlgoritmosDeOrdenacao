2299017f6b2de15cd03e105a9e243ba3eadd74cf
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-11-10 17:22:58 -0300

    Agora o método 'particao' segue a risca a implementação original.

--- src/QuickSort.py
+++ src/QuickSort.py
@@ -1,5 +1,5 @@
 class QuickSort():
-
+    
     def quick_sort(self, lista_numeros, inicio: int, fim: int):
         if inicio < fim:
             posPivo = self.particao(lista_numeros, inicio, fim)
@@ -10,19 +10,20 @@ class QuickSort():
  
     def particao(self, lista_numeros, inicio: int, fim: int):
         pivo = lista_numeros[fim]
-        i = inicio
+        i = inicio - 1
         for j in range(inicio, fim):
             # j sempre avança, pois representa o elemento em análise
             # e delimita os elementos maiores que o pivô
             if lista_numeros[j] <= pivo:
+                # incrementa-se o limite dos elementos menores que o pivô
+                i = i + 1
+
                 tmp = lista_numeros[j]
                 lista_numeros[j] = lista_numeros[i]
                 lista_numeros[i] = tmp
-                # incrementa-se o limite dos elementos menores que o pivô
-                i = i + 1
-        
-        tmp = lista_numeros[i]
-        lista_numeros[i] = lista_numeros[fim]
+                
+        tmp = lista_numeros[i + 1]
+        lista_numeros[i + 1] = lista_numeros[fim]
         lista_numeros[fim] = tmp
         
-        return i
+        return i + 1
